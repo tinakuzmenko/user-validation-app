3ab1ccd908f59835337b93e16dc157dd776b7265
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2021-11-18 23:07:00 +0100

    Adding user functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+import {useState} from 'react';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (name, age) => {
+    setUsersList((prevState) => [...prevState, { name, age, id: Math.random().toString() }]);
+  }
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,19 +1,39 @@
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import styles from './AddUser.module.css';
+import {useState} from 'react';
 
 const AddUser = (props) => {
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if (!username.trim().length || !age.trim().length) return;
+    if (+age < 1) return;
+
+    props.onAddUser(username, age);
+
+    setUsername('');
+    setAge('');
+  };
+
+  const usernameChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setAge(event.target.value);
   }
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">User</label>
-        <input id="username" type="text"/>
+        <input id="username" type="text" onChange={usernameChangeHandler} value={username} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"/>
+        <input id="age" type="number" onChange={ageChangeHandler} value={age} />
         <Button type="submit">Add user</Button>
       </form>
     </Card>
