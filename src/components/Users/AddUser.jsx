7303f5de4967a8c9447024dd7b20dc999ed62ec6
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2021-11-18 22:32:09 +0100

    Add AddUser component and basic styles

---
+++ src/components/Users/AddUser.jsx
@@ -0,0 +1,23 @@
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import styles from './AddUser.module.css';
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">User</label>
+        <input id="username" type="text"/>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"/>
+        <Button type="submit">Add user</Button>
+      </form>
+    </Card>
+  )
+};
+
+export default AddUser;
